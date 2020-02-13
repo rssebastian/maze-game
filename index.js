88f540a6cc3ac217e52a45dbbefe5bd96ad335ec
EC2 Default User <ec2-user@ip-172-31-18-226.us-west-2.compute.internal>
2020-02-13 19:02:46 +0000

    eliminated demo code and returned to creating borders

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const width = 800;
+const width = 600;
 const height = 600;
 
 // Creates new engine
@@ -14,7 +14,7 @@ const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        wireframes: false,
+        wireframes: true,
         width,
         height
     }
@@ -26,31 +26,12 @@ Render.run(render);
 // Start the Runner, connects world to engine
 Runner.run(Runner.create(), engine)
 
-World.add(world, MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas)
-}))
-
 // Walls
 const walls = [
-    Bodies.rectangle( 400, 0, 800, 40, {isStatic: true} ),
-    Bodies.rectangle( 400, 600, 800, 40, {isStatic: true} ),
-    Bodies.rectangle( 0, 300, 40, 600, {isStatic: true} ),
-    Bodies.rectangle( 800, 300, 40, 600, {isStatic: true} )
+    Bodies.rectangle( width/2, 0, width, 40, {isStatic: true} ),
+    Bodies.rectangle( width/2, height, width, 40, {isStatic: true} ),
+    Bodies.rectangle( 0, height/2, 40, height, {isStatic: true} ),
+    Bodies.rectangle( width, height/2, 40, height, {isStatic: true} )
 ];
 World.add(world, walls);
-
-// Random Shapes
-for (let i=0; i<20; i++) {
-    if (Math.random() > 0.5) {
-        World.add(
-            world, 
-            Bodies.rectangle( Math.random() * width, Math.random() * height, 40, 40)
-        );
-    } else {
-        World.add(
-            world, 
-            Bodies.circle( Math.random() * width, Math.random() * height, 40, 40)
-        );
-    };
-};
 
