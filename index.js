c69929634d948f2b429cf83bfc963b6ec62d1a40
EC2 Default User <ec2-user@ip-172-31-18-226.us-west-2.compute.internal>
2020-02-19 23:29:10 +0000

    added logic to render the ball and goal

--- index.js
+++ index.js
@@ -32,10 +32,10 @@ Runner.run(Runner.create(), engine)
 
 // Walls
 const walls = [
-    Bodies.rectangle( width/2, 0, width, 40, {isStatic: true} ),
-    Bodies.rectangle( width/2, height, width, 40, {isStatic: true} ),
-    Bodies.rectangle( 0, height/2, 40, height, {isStatic: true} ),
-    Bodies.rectangle( width, height/2, 40, height, {isStatic: true} )
+    Bodies.rectangle( width/2, 0, width, 2, {isStatic: true} ),
+    Bodies.rectangle( width/2, height, width, 2, {isStatic: true} ),
+    Bodies.rectangle( 0, height/2, 2, height, {isStatic: true} ),
+    Bodies.rectangle( width, height/2, 2, height, {isStatic: true} )
 ];
 World.add(world, walls);
 
@@ -178,3 +178,22 @@ verticals.forEach((row, rowIndex) => {
         World.add(world, wall);
     });
 });
+
+// Goal
+
+const goal = Bodies.rectangle(
+    width - unitLength / 2,
+    height - unitLength / 2,
+    unitLength * 0.7,
+    unitLength * 0.7, 
+    { isStatic: true }
+);
+World.add(world, goal);
+
+// Ball
+const ball = Bodies.circle(
+    unitLength / 2,
+    unitLength / 2,
+    unitLength * 0.25
+);
+World.add(world, ball);
